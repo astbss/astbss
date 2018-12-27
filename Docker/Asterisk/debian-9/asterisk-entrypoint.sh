25b92ae34a39f4f964a1f01e9cad917e07543d42
Are Casilla <are@astbss.com>
2018-12-27 10:07:02 +0100

    Asterisk Entrypoint

--- Docker/Asterisk/debian-9/asterisk-entrypoint.sh
+++ Docker/Asterisk/debian-9/asterisk-entrypoint.sh
@@ -13,6 +13,9 @@ set -e
 # -cpuset-cpus="0,1" may be better than nice
 # https://docs.docker.com/config/containers/resource_constraints/#understand-the-risks-of-running-out-of-memory
 # Using --cap-add=SYS_NICE worked
+
+# https://github.com/docker-library/php
+
 exec "nice -n 0 /usr/sbin/asterisk -f vvv"
  
 # exec "$@"
