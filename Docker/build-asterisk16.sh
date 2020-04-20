1a90a9e0546fcf8a8009d06e318ae9f6f962ab45
Are Casilla <are@astbss.com>
2020-04-20 13:48:56 +0200

    asterisk 16

--- Docker/build-asterisk16.sh
+++ Docker/build-asterisk16.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+# Thanks to https://andrius.mobi/ Andrius Kairiukstis
+# https://github.com/andrius/asterisk
 # https://github.com/andrius/asterisk/blob/master/debian/16-current/build-asterisk.sh
+
 PROGNAME=$(basename $0)
 
 if test -z ${ASTERISK_VERSION}; then
@@ -11,19 +15,51 @@ set -ex
 
 useradd --system asterisk
 
+apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends --no-install-suggests \
+    autoconf \
+    binutils-dev \
+    build-essential \
+    ca-certificates \
+    curl \
+    file \
+    libcurl4-openssl-dev \
+    libedit-dev \
+    libgsm1-dev \
+    libogg-dev \
+    libpopt-dev \
+    libresample1-dev \
+    libspandsp-dev \
+    libspeex-dev \
+    libspeexdsp-dev \
+    libsqlite3-dev \
+    libsrtp2-dev \
+    libssl-dev \
+    libvorbis-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    procps \
+    portaudio19-dev \
+    unixodbc \
+    unixodbc-bin \
+    unixodbc-dev \
+    odbcinst \
+    uuid \
+    uuid-dev \
+    xmlstarlet
 
 
-# apt-get purge -y --auto-remove
-# rm -rf /var/lib/apt/lists/*
+apt-get purge -y --auto-remove
+rm -rf /var/lib/apt/lists/*
 
 
 
 mkdir -p /usr/src/asterisk
 cd /usr/src/asterisk
 
-#curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz || \
-#curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz || \
-#curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz
+curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz || \
+curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz || \
+curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz
 
 # 1.5 jobs per core works out okay
 : ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
@@ -96,7 +132,7 @@ make install
 
 # copy default configs
 # cp /usr/src/asterisk/configs/basic-pbx/*.conf /etc/asterisk/
-# make samples
+make samples
 
 # set runuser and rungroup
 # sed -i -E 's/^;(run)(user|group)/\1\2/' /etc/asterisk/asterisk.conf
