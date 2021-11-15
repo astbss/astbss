31885886fc212afd36f360962de6629e137ce4b6
Are Casilla <astbss@users.noreply.github.com>
2021-11-15 07:09:08 +0100

    Update build3.7.sh

--- Docker-Python/build3.7.sh
+++ Docker-Python/build3.7.sh
@@ -66,7 +66,7 @@ pip install libgravatar htpasswd python-digitalocean py_daemon
 pip install linode-api4 schedule pycryptodome msrestazure haikunator azure-mgmt-resource
 pip install hcloud flask_caching python-jwt
 pip install asyncio aiohttp uvloop
-pip install async-timeout
+pip install async-timeout asyncio-periodic
 
 
 # https://www.psycopg.org/docs/install.html#install-from-source
