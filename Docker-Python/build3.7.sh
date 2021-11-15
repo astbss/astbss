1edea0aa9464cab2a4d7d52d1ad062281be1b021
Are Casilla <astbss@users.noreply.github.com>
2021-11-15 07:14:57 +0100

    Update build3.7.sh

--- Docker-Python/build3.7.sh
+++ Docker-Python/build3.7.sh
@@ -66,7 +66,7 @@ pip install libgravatar htpasswd python-digitalocean py_daemon
 pip install linode-api4 schedule pycryptodome msrestazure haikunator azure-mgmt-resource
 pip install hcloud flask_caching python-jwt
 pip install aiohttp[speedups]
-pip install asyncio aiohttp uvloop
+pip install asyncio uvloop
 pip install async-timeout asyncio-periodic
 
 
