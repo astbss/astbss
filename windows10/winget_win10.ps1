35b1219d62ee8e84892352b48cc710bec294178e
Are Casilla <are@astbss.com>
2021-11-19 16:22:27 +0100

    Windows 10

--- windows10/winget_win10.ps1
+++ windows10/winget_win10.ps1
@@ -1,4 +1,10 @@
 # Run as admin
+# You must be running Windows 10 version 2004 and higher
+# (Build 19041 and higher) or Windows 11.
+# winver
+# 1st Upgrade your windows so you get full latest version
+
+wsl --set-default-version 2
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 # run as user
