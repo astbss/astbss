673891a4211813231d20f48cb83551b662cde240
Are Casilla <are@astbss.com>
2021-11-19 16:11:19 +0100

    Windows 10

---
+++ windows10/winget_win10.ps1
@@ -0,0 +1,6 @@
+# Run as admin
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+
+# run as user
+winget install Microsoft.WindowsTerminal
+winget install  Google.Chrome
