707555c569d6d48dbd65c1703963efcab840c9bf
Are Casilla <are@astbss.com>
2021-12-22 08:30:26 +0100

    build3.9

--- windows10/winget_win10.ps1
+++ windows10/winget_win10.ps1
@@ -4,9 +4,19 @@
 # winver
 # 1st Upgrade your windows so you get full latest version
 
+wsl --install
 wsl --set-default-version 2
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+# Windows Linux Kernel Upgrade
+# https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
+
+# Open appstore
+# https://www.microsoft.com/store/apps/9n6svws3rx71
 
 # run as user
 winget install Microsoft.WindowsTerminal
 winget install  Google.Chrome
+winget install -e --id LibreOffice.LibreOffice
