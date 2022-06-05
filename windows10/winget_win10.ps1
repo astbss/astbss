10430d4e9ec75cb32f2c705114cbacd69f7ac1d4
Are Casilla <astbss@users.noreply.github.com>
2022-06-06 00:56:33 +0200

    Update winget_win10.ps1

--- windows10/winget_win10.ps1
+++ windows10/winget_win10.ps1
@@ -24,6 +24,7 @@ dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux 
 # https://ss64.com/nt/winget-install.html
 winget install Microsoft.WindowsTerminal
 winget install --accept-package-agreements --accept-source-agreements Google.Chrome
+winget install --accept-source-agreements --id Git.Git -e    
 winget install -e --id LibreOffice.LibreOffice
 winget install brackets-cont.brackets
 winget install Skillbrains.lightshot
