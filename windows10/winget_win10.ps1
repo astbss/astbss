2d31824d8bef932ac4457bbdad5e03101b002a02
Are Casilla <astbss@users.noreply.github.com>
2022-06-06 00:36:01 +0200

    Update winget_win10.ps1

--- windows10/winget_win10.ps1
+++ windows10/winget_win10.ps1
@@ -6,7 +6,7 @@
 
 There is a single command that will install both WSL and Ubuntu at the same time.
 wsl --install -d ubuntu
-# Below commands are optional and not needed for wsl
+# Below commands are not needed for wsl anymore
 wsl --install
 Restart-Computer
 wsl --set-default-version 2
