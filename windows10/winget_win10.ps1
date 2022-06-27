b0316d150bb4eaafa4462bcc84d87c3b020340f8
Are Casilla <astbss@users.noreply.github.com>
2022-06-27 06:22:19 +0200

    Update winget_win10.ps1

--- windows10/winget_win10.ps1
+++ windows10/winget_win10.ps1
@@ -28,6 +28,7 @@ winget install -e --id LibreOffice.LibreOffice
 winget install brackets-cont.brackets
 winget install Skillbrains.lightshot
 winget install --id=Skillbrains.lightshot  -e
+winget install -e --id Discord.Discord
 
 winget install --accept-source-agreements -e --id Microsoft.VisualStudioCode
 Restart Ubuntu and Powershell windows for code to appear in the path
