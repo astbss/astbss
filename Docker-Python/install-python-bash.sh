823c355bb7b5c22d2dbc09e3d7e3cf19537634aa
Are Casilla <astbss@users.noreply.github.com>
2022-08-09 12:18:08 +0200

    Update install-python-bash.sh

--- Docker-Python/install-python-bash.sh
+++ Docker-Python/install-python-bash.sh
@@ -6,6 +6,7 @@ sudo apt install software-properties-common
 
 echo Adding deadsnakes/ppa
 sudo add-apt-repository --yes ppa:deadsnakes/ppa
+sudo apt update
 
 echo Install software-properties-common
 sudo apt install -y software-properties-common build-essential zlib1g-dev libncurses5-dev \
@@ -43,7 +44,5 @@ sudo apt-get install -y python3.9-dev
 echo Installing python 3.10
 sudo apt install python3.10 -y
 sudo apt install python3.10-distutils -y
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 sudo apt install python3.10-venv
-python3.10 -m pip install --user --upgrade pip
 sudo apt-get install -y python3.10-dev
