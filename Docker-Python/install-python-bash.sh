8ffdb399f9d2a1c439b264c24c1f57fffac84a11
Are Casilla <astbss@users.noreply.github.com>
2023-04-02 20:58:53 +0200

    Update install-python-bash.sh

--- Docker-Python/install-python-bash.sh
+++ Docker-Python/install-python-bash.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root or with sudo! Exiting ...' >&2
+        exit 1
+fi
+
 sudo apt update
 sudo apt install software-properties-common
 
