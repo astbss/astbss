7353a883c730e22e187cba7f8261f416b82b794e
Are Casilla <are@astbss.com>
2025-04-18 16:04:52 +0200

    python

--- Docker-Python/install-python-bash.sh
+++ Docker-Python/install-python-bash.sh
@@ -43,11 +43,19 @@ sudo apt install -y software-properties-common build-essential zlib1g-dev libncu
 
 # Ubuntu 22.04 already have Python 3.10
 
-echo Installing python 3.9
-sudo apt install python3.9 -y
-sudo apt install -y python3-pip -y
-sudo apt install python3.9-venv -y
-sudo apt install -y python3.9-dev -y
+# echo Installing python 3.9
+# sudo apt install python3.9 -y
+# sudo apt install -y python3-pip -y
+# sudo apt install python3.9-venv -y
+# sudo apt install -y python3.9-dev -y
+
+
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+# Make sure to restart your terminal afterwards to ensure that the uv command gets picked up.
+# uv python list
+# uv venv .venv3.13 --python 3.13.3
+# source .venv3.13/bin/activate
+# python3 -V
 
 echo Installing python 3.11
 sudo apt install python3.11 -y
@@ -55,5 +63,12 @@ sudo apt install python3.11-distutils -y
 sudo apt install python3.11-venv -y
 sudo apt install -y python3.11-dev
 
-python3.9 -V
+echo Installing python 3.13
+sudo apt install python3.13 -y
+sudo apt install python3.13-distutils -y
+sudo apt install python3.13-venv -y
+sudo apt install -y python3.13-dev
+
+# python3.9 -V
 python3.11 -V
+python3.13 -V
